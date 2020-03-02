0f13c10015a5463b6b587530500a08a473e18192
allenwang <zbxhui@gmail.com>
2020-03-02 09:58:30 +0800

    新增RestTemplate

---
+++ rest-template/src/main/java/com/bestboke/springcloud/resttemplate/RestTemplateApplication.java
@@ -0,0 +1,15 @@
+package com.bestboke.springcloud.resttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class RestTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplateApplication.class, args);
+    }
+
+}
---
+++ rest-template/src/main/java/com/bestboke/springcloud/resttemplate/controller/IndexController.java
@@ -0,0 +1,50 @@
+package com.bestboke.springcloud.resttemplate.controller;
+
+import com.bestboke.springcloud.resttemplate.pojo.Info;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class IndexController {
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    @RequestMapping("/")
+    public String index() {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ServiceInstance serviceInstance = loadBalancerClient.choose("API");
+        String url = String.format("http://%s:%s", serviceInstance.getHost(), serviceInstance.getPort());
+        String response = restTemplate.getForObject(url, String.class);
+        return response;
+
+    }
+
+    @RequestMapping("/info")
+    public Info info() {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ServiceInstance serviceInstance = loadBalancerClient.choose("API");
+        String url = String.format("http://%s:%s/info", serviceInstance.getHost(), serviceInstance.getPort());
+        Info response = restTemplate.getForObject(url, Info.class);
+        return response;
+
+    }
+
+    @RequestMapping("info-new")
+    public Info infoNew(){
+        return restTemplate.getForObject("http://API/info", Info.class);
+    }
+
+}
