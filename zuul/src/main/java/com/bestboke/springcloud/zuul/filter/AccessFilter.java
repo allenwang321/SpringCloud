a55ea315abcd5ead49d7941516701f603d9b9583
allenwang <zbxhui@gmail.com>
2020-03-17 16:21:53 +0800

    新增zuul,修改api

---
+++ zuul/src/main/java/com/bestboke/springcloud/zuul/filter/AccessFilter.java
@@ -0,0 +1,39 @@
+//package com.bestboke.springcloud.zuul.filter;
+//
+//import com.netflix.zuul.ZuulFilter;
+//import com.netflix.zuul.context.RequestContext;
+//import com.netflix.zuul.exception.ZuulException;
+//import org.springframework.stereotype.Component;
+//
+//import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
+//import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+//
+//@Component
+//public class AccessFilter extends ZuulFilter {
+//
+//
+//    @Override
+//    public String filterType() {
+//        return PRE_TYPE;
+//    }
+//
+//    @Override
+//    public int filterOrder() {
+//        return PRE_DECORATION_FILTER_ORDER -1;
+//    }
+//
+//    @Override
+//    public boolean shouldFilter() {
+//        return true;
+//    }
+//
+//    @Override
+//    public Object run() throws ZuulException {
+//        RequestContext ctx = RequestContext.getCurrentContext();
+//        String sessionId = ctx.getRequest().getSession().getId();
+//        ctx.addZuulRequestHeader("Cookie", "SESSIONID=" + sessionId);
+//        ctx.setSendZuulResponse(true);// 对该请求进行路由
+//        ctx.setResponseStatusCode(200);
+//        return null;
+//    }
+//}
